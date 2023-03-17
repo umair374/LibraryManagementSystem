adba50112fb9236b205aaa7069a8b677223db8a3
umair374 <umair.shahid644@gmail.com>
2023-03-17 19:41:38 +0500

    SQL Connection +Creating Table using sequelize

---
+++ models/userModel.js
@@ -0,0 +1,55 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(
+ 'umair',
+ 'root',
+ '',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
+
+const user = sequelize.define("users", {
+   name: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },    
+   registered_books: {
+     type: DataTypes.INTEGER,
+   },
+   fine: {
+     type: DataTypes.INTEGER,
+   }
+});
+
+sequelize.sync().then(() => {
+   console.log('user table created successfully!');
+
+   user.create({
+       name: "Ali",
+       id: 123,
+       registered_books: 3,
+       fine: 500
+   }).then(res => {
+       console.log(res)
+   }).catch((error) => {
+       console.error('Failed to create a new record : ', error);
+   });
+
+}).catch((error) => {
+   console.error('Unable to create table : ', error);
+});
+
+module.exports = user;
--- server.js
+++ server.js
@@ -1,6 +1,45 @@
 const express = require("express");
 const app=express();
 const indexRouters = require("./routers/index.routers");
+const { user } = require("./models/userModel");
+
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'umair'
+});
+ 
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected ");
+});
+
+connection.query(
+  "SELECT * FROM users ",
+  function (error, results, fields) {
+    if (error) throw error;
+    res = results;
+    console.log("Registered users : ", results);
+  }
+);
+
+connection.end();
+// user.findAll().then((results) => {
+//   console.log("Registered users : ", results);
+// }).catch((error) => {
+//   console.error('Failed to retrieve users : ', error);
+// });
+
+
+
+
 app.use("/",indexRouters);
 
 app.listen("3000");
